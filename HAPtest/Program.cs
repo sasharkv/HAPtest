51dd076216622cb82fe10c73329248763a11705c
sasha radkova <sasharadkova@outlook.com>
2022-06-30 11:54:46 +0200

    Add project files.

---
+++ HAPtest/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using HtmlAgilityPack;
+
+HtmlWeb web = new HtmlWeb(); // we can use this object to load the page
+HtmlDocument document = web.Load("https://example.com/");
+
+// we can ge the Title by hetting th h1
+var title = document.DocumentNode.SelectNodes("//div/h1").First().InnerText;
+var description = document.DocumentNode.SelectNodes("//div/p").First().InnerText;
+
+Console.WriteLine(title);
+Console.WriteLine();
+Console.WriteLine(description);
+
+Console.ReadLine(); 
+
+
